e84ca9fdec64b37c1a26995532eaa95c54710f2d
Daniel Radetsky <dmr@parthenonsoftware.com>
2012-06-13 20:33:17 -0700

    Initial Import.

---
+++ redef.lisp
@@ -0,0 +1,35 @@
+;; (defpackage :redef 
+;;   (use :cl :sb-mop))
+
+;; (in-package :redef)
+
+use-package
+
+(defmacro def (name expr &rest body)
+  (if (null body)
+      `(defparameter ,name ,expr)
+      `(defmethod ,name ,expr ,@body)
+      ))
+
+(defmacro cls (name super &rest slots)
+  (let ((processed-slots (mapcan (lambda (slot-name)
+                                   (let ((slot-keyword (values (intern (string-upcase (symbol-name slot-name))
+                                                                       "KEYWORD"))))
+                                     `((,slot-name :initarg ,slot-keyword :accessor ,slot-keyword)))) slots)))
+    `(defclass ,name ,super ,processed-slots)))
+
+
+
+;; (def foo (x)
+;;   (+ 2 x))
+
+;; (def foo ((x cons))
+;;   (cons 2 x))
+
+(cls cc () 
+       x y)
+
+;(def c (obj 'cc 1 2))
+
+(def c (make-instance 'cc :x 1 :y 2))
+
